856c63889ca1de01ffb950d520893e0fa6b2f9bc
Sairaghava <164220290+sairaghava2542@users.noreply.github.com>
2026-05-02 12:49:04 +0530

    Update run_dc.tcl

--- run_dc.tcl
+++ run_dc.tcl
@@ -1,39 +1,29 @@
 
 set target_library ./../ref/lib/stdcell_rvt/saed32rvt_tt0p78vn40c.db 
-
-      set link_library ./../ref/lib/stdcell_rvt/saed32rvt_tt0p78vn40c.db
+set link_library ./../ref/lib/stdcell_rvt/saed32rvt_tt0p78vn40c.db
        
-       set RTL_SOURCE_FILES ./../design/fifo.v
+set RTL_SOURCE_FILES ./../design/fifo.v
+set DESIGN_NAME fifo
 
-	set DESIGN_NAME fifo
-
-       define_design_lib WORK -path ./WORK
-       set_app_var hdlin_enable_hier_map true
-       analyze -format verilog ${RTL_SOURCE_FILES}
-       elaborate ${DESIGN_NAME}
-      link
-      
+define_design_lib WORK -path ./WORK
+set_app_var hdlin_enable_hier_map true
+analyze -format verilog ${RTL_SOURCE_FILES}
+elaborate ${DESIGN_NAME}
+link      
 current_design
-    
 read_sdc ./../design/fifo.sdc
 
-      
-      
-      
-      set_verification_top
-
-
-      
+set_verification_top     
 complie_ultra 
-      report_timing -delay_type max
-      report_timing -delay_type min
+report_timing -delay_type max
+report_timing -delay_type min
       
-      report_timing -delay_type min > hold.rpt
+report_timing -delay_type min > hold.rpt
       
-      report_timing -delay_type max > setup.rpt
+report_timing -delay_type max > setup.rpt
      
-      report_qor > qor.rpt
-      report_area > area.rpt
-      report_power > power.rpt
+report_qor > qor.rpt
+report_area > area.rpt
+report_power > power.rpt
 
 
